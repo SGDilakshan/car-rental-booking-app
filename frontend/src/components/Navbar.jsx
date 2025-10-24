2c6d8c96b15ceb99fcab91ef2e6551a115aa5cba
SGDilakshan <sgd.dilakshan@gmail.com>
2025-10-25 00:44:22 +0530

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
-import {Link, useLocation} from "react-router-dom"
+import {Link, useLocation, useNavigate} from "react-router-dom"
 import { assets, menuLinks } from '../assets/assets'
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
     const location = useLocation()
     const [open,setOpen] = useState(false)
+    const navigate = useNavigate()
 
 
   return (
@@ -24,7 +25,16 @@ const Navbar = () => {
                 <input type="text" className='py-1.5 w-full bg-transparent outline-none placeholder-gray-500' placeholder="Search products" />
                 <img src={assets.search_icon} alt="search" />
             </div>
+
+            <div className='flex max-sm:flex-col items-start sm:items-center gap-6'>
+                <button onClick={() => navigate("/owner")} className='cursor-pointer'>Dashboard</button>
+                <button onClick={() => setShowLogin(true)} className='cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg'>Login</button>
+            </div>
         </div>
+
+        <button className='sm:hidden cursor-pointer' aria-label='Menu' onClick={() => setOpen (!open)}>
+            <img src={open ? assets.close_icon : assets.menu_icon} alt="menu" />
+        </button>
         
     </div>
   )
